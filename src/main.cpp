994e39b16dc38cc12953924ea580f41a9ec5feb9
Mykola <nikolaysedovlasiy@gmail.com>
2025-05-04 14:06:11 +0300

    Added cmake project, glwf, glad.

---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW\n";
+        return -1;
+    }
+
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GLAD + GLFW", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+
+    if (!gladLoadGL()) {
+        std::cerr << "Failed to initialize GLAD\n";
+        return -1;
+    }
+
+
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << '\n';
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
+
